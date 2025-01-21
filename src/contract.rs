loreps/airdrop-demo

Create initial project using the Linera SDK
The goal is to implement an Airdrop demo application using a ZK proof
generated by Space and Time to claim the tokens.

src/contract.rs
@@ -1,0 +1,45 @@
+#![cfg_attr(target_arch = "wasm32", no_main)]
+
+mod state;
+
+use linera_sdk::{
+    base::WithContractAbi,
+    views::{RootView, View, ViewStorageContext},
+    Contract, ContractRuntime,
+};
+
+use self::state::Application;
+
+pub struct ApplicationContract {
+    state: Application,
+    runtime: ContractRuntime<Self>,
+}
+
+linera_sdk::contract!(ApplicationContract);
+
+impl WithContractAbi for ApplicationContract {
+    type Abi = airdrop_demo::ApplicationAbi;
+}
+
+impl Contract for ApplicationContract {
+    type Message = ();
+    type Parameters = ();
+    type InstantiationArgument = ();
+
+    async fn load(runtime: ContractRuntime<Self>) -> Self {
+        let state = Application::load(ViewStorageContext::from(runtime.key_value_store()))
+            .await
+            .expect("Failed to load state");
+        ApplicationContract { state, runtime }
+    }
+
+    async fn instantiate(&mut self, _argument: Self::InstantiationArgument) {}
+
+    async fn execute_operation(&mut self, _operation: Self::Operation) -> Self::Response {}
+
+    async fn execute_message(&mut self, _message: Self::Message) {}
+
+    async fn store(mut self) {
+        self.state.save().await.expect("Failed to save state");
+    }
+}

src/service.rs
@@ -1,0 +1,36 @@
+#![cfg_attr(target_arch = "wasm32", no_main)]
+
+mod state;
+
+use self::state::Application;
+use linera_sdk::{
+    base::WithServiceAbi,
+    views::{View, ViewStorageContext},
+    Service, ServiceRuntime,
+};
+
+pub struct ApplicationService {
+    state: Application,
+    runtime: ServiceRuntime<Self>,
+}
+
+linera_sdk::service!(ApplicationService);
+
+impl WithServiceAbi for ApplicationService {
+    type Abi = airdrop_demo::ApplicationAbi;
+}
+
+impl Service for ApplicationService {
+    type Parameters = ();
+
+    async fn new(runtime: ServiceRuntime<Self>) -> Self {
+        let state = Application::load(ViewStorageContext::from(runtime.key_value_store()))
+            .await
+            .expect("Failed to load state");
+        ApplicationService { state, runtime }
+    }
+
+    async fn handle_query(&self, _query: Self::Query) -> Self::QueryResponse {
+        panic!("Queries not supported by application");
+    }
+}

linera-io/atoma-demo

src/contract.rs
@@ -1,0 +1,45 @@
+#![cfg_attr(target_arch = "wasm32", no_main)]
+
+mod state;
+
+use linera_sdk::{
+    base::WithContractAbi,
+    views::{RootView, View, ViewStorageContext},
+    Contract, ContractRuntime,
+};
+
+use self::state::Application;
+
+pub struct ApplicationContract {
+    state: Application,
+    runtime: ContractRuntime<Self>,
+}
+
+linera_sdk::contract!(ApplicationContract);
+
+impl WithContractAbi for ApplicationContract {
+    type Abi = atoma_demo::ApplicationAbi;
+}
+
+impl Contract for ApplicationContract {
+    type Message = ();
+    type Parameters = ();
+    type InstantiationArgument = ();
+
+    async fn load(runtime: ContractRuntime<Self>) -> Self {
+        let state = Application::load(ViewStorageContext::from(runtime.key_value_store()))
+            .await
+            .expect("Failed to load state");
+        ApplicationContract { state, runtime }
+    }
+
+    async fn instantiate(&mut self, _argument: Self::InstantiationArgument) {}
+
+    async fn execute_operation(&mut self, _operation: Self::Operation) -> Self::Response {}
+
+    async fn execute_message(&mut self, _message: Self::Message) {}
+
+    async fn store(mut self) {
+        self.state.save().await.expect("Failed to save state");
+    }
+}

src/service.rs
@@ -1,0 +1,36 @@
+#![cfg_attr(target_arch = "wasm32", no_main)]
+
+mod state;
+
+use self::state::Application;
+use linera_sdk::{
+    base::WithServiceAbi,
+    views::{View, ViewStorageContext},
+    Service, ServiceRuntime,
+};
+
+pub struct ApplicationService {
+    state: Application,
+    runtime: ServiceRuntime<Self>,
+}
+
+linera_sdk::service!(ApplicationService);
+
+impl WithServiceAbi for ApplicationService {
+    type Abi = atoma_demo::ApplicationAbi;
+}
+
+impl Service for ApplicationService {
+    type Parameters = ();
+
+    async fn new(runtime: ServiceRuntime<Self>) -> Self {
+        let state = Application::load(ViewStorageContext::from(runtime.key_value_store()))
+            .await
+            .expect("Failed to load state");
+        ApplicationService { state, runtime }
+    }
+
+    async fn handle_query(&self, _query: Self::Query) -> Self::QueryResponse {
+        panic!("Queries not supported by application");
+    }
+}