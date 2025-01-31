239cc3d3e0f5574bcf9e6e12a4b2c33f2d127a70
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2025-01-31 19:34:08 +0000

    Add a mutation to perform a chat interaction
    Creates an operation to log a chat interaction, but for now the response
    is empty.

--- src/service.rs
+++ src/service.rs
@@ -7,8 +7,10 @@ mod state;
 
 use std::sync::{Arc, Mutex};
 
-use async_graphql::{connection::EmptyFields, EmptyMutation, EmptySubscription, Schema};
-use linera_sdk::{base::WithServiceAbi, Service, ServiceRuntime};
+use async_graphql::{connection::EmptyFields, EmptySubscription, Schema};
+use atoma_demo::{ChatInteraction, Operation};
+use linera_sdk::{base::WithServiceAbi, bcs, Service, ServiceRuntime};
+use serde::{Deserialize, Serialize};
 
 #[derive(Clone)]
 pub struct ApplicationService {
@@ -31,9 +33,49 @@ impl Service for ApplicationService {
     }
 
     async fn handle_query(&self, query: Self::Query) -> Self::QueryResponse {
-        Schema::build(EmptyFields, EmptyMutation, EmptySubscription)
-            .finish()
-            .execute(query)
-            .await
+        Schema::build(
+            EmptyFields,
+            Mutation {
+                runtime: self.runtime.clone(),
+            },
+            EmptySubscription,
+        )
+        .finish()
+        .execute(query)
+        .await
     }
+}
+
+/// Root type that defines all the GraphQL mutations available from the service.
+pub struct Mutation {
+    runtime: Arc<Mutex<ServiceRuntime<ApplicationService>>>,
+}
+
+#[async_graphql::Object]
+impl Mutation {
+    /// Executes a chat completion using the Atoma Network.
+    async fn chat(
+        &self,
+        api_token: String,
+        message: ChatMessage,
+    ) -> async_graphql::Result<Vec<u8>> {
+        let interaction = ChatInteraction {
+            prompt: message.content,
+            response: "".to_owned(),
+        };
+
+        Ok(
+            bcs::to_bytes(&Operation::LogChatInteraction { interaction })
+                .expect("`LogChatInteraction` should be serializable"),
+        )
+    }
+}
+
+/// A message to be sent to the AI chat.
+#[derive(Clone, Debug, Deserialize, Serialize, async_graphql::InputObject)]
+pub struct ChatMessage {
+    content: String,
+    role: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    name: Option<String>,
 }
