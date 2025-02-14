3a7d8f18a8dd51b89f93c5d1c4961678461a4e61
Janito Vaqueiro Ferreira Filho <janito.vff@gmail.com>
2025-02-14 19:36:24 +0000

    Test if chat log can be inspected with GraphQL
    Ensure that the chat log stored on chain can be read through GraphQL.

--- src/service_unit_tests.rs
+++ src/service_unit_tests.rs
@@ -2,11 +2,74 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use atoma_demo::{ChatInteraction, Operation};
-use linera_sdk::{bcs, http, util::BlockingWait, Service, ServiceRuntime};
+use linera_sdk::{
+    bcs, http,
+    util::BlockingWait,
+    views::{RootView, View},
+    Service, ServiceRuntime, ViewStorageContext,
+};
 use serde_json::json;
 use test_strategy::proptest;
 
-use super::{ApplicationService, ATOMA_CLOUD_URL};
+use super::{state::Application, ApplicationService, ATOMA_CLOUD_URL};
+
+/// Tests if the chat logged on chain can be inspected with GraphQL.
+#[proptest]
+fn read_chat_log(interactions: Vec<ChatInteraction>) {
+    let runtime = ServiceRuntime::new();
+    let storage = runtime.key_value_store().to_mut();
+
+    let mut initial_state = Application::load(ViewStorageContext::new_unsafe(storage, vec![], ()))
+        .blocking_wait()
+        .expect("Failed to load state from mock storage");
+
+    for interaction in interactions.iter().cloned() {
+        initial_state.chat_log.push(interaction);
+    }
+
+    initial_state
+        .save()
+        .blocking_wait()
+        .expect("Failed to save initial state to mock storage");
+
+    let service = setup_service(runtime);
+
+    let request = async_graphql::Request::new("query { chatLog { entries { prompt, response } } }");
+
+    let response = service.handle_query(request).blocking_wait();
+
+    let async_graphql::Value::Object(response_data) = response.data else {
+        panic!("Unexpected response data type");
+    };
+    let async_graphql::Value::Object(ref chat_log) = response_data["chatLog"] else {
+        panic!("Unexpected response chat log type");
+    };
+    let async_graphql::Value::List(ref entries) = chat_log["entries"] else {
+        panic!("Unexpected response entries type");
+    };
+
+    let persisted_interactions = entries
+        .iter()
+        .map(|entry_value| {
+            let async_graphql::Value::Object(entry) = entry_value else {
+                panic!("Unexpected interaction entry type");
+            };
+            let async_graphql::Value::String(ref prompt) = entry["prompt"] else {
+                panic!("Unexpected interaction prompt type");
+            };
+            let async_graphql::Value::String(ref response) = entry["response"] else {
+                panic!("Unexpected interaction response type");
+            };
+
+            ChatInteraction {
+                prompt: prompt.clone(),
+                response: response.clone(),
+            }
+        })
+        .collect::<Vec<_>>();
+
+    assert_eq!(persisted_interactions, interactions);
+}
 
 /// Tests if `chat` mutations perform an HTTP request to the Atoma proxy, and generates the
 /// operation to log a chat interaction.
